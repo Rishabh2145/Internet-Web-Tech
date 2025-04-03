af66feaac8281809c0d37a7758629f07762bea25
Rishabh Kumar <rishabrbl2005@gmail.com>
2025-04-04 00:16:41 +0530

    new

--- login.js
+++ login.js
@@ -1,8 +1,11 @@
 var back = document.getElementById("back");
+var top = document.getElementById("top");
+var down = document.getElementById("down");
 var newAcc = document.getElementById("newAcc");
 var cred = document.getElementById("cred");
 var reg = document.getElementById("reg");
 var register = document.getElementById("insert");
+
 
 function back_login()
 {
