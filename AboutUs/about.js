8dd94ab6fb5c6d1a106de33934d862170b9247e3
Rishabh Kumar <rishabrbl2005@gmail.com>
2025-04-27 12:49:57 +0530

    mobile updates

--- AboutUs/about.js
+++ AboutUs/about.js
@@ -1,26 +1,67 @@
 var menu_icon = document.getElementById("menu-hover");
 var hover_icon = document.getElementById("hover");
-menu_icon.addEventListener('mouseover', () => {
+
+// Function to set event listeners
+function setupListeners() {
+    if (window.innerWidth <= 768) { // Mobile: Click behavior
+        menu_icon.addEventListener('click', toggleMenu);
+        hover_icon.addEventListener('click', toggleMenu);
+
+        // Click outside to close
+        document.addEventListener('click', (event) => {
+            if (!menu_icon.contains(event.target) && !hover_icon.contains(event.target)) {
+                closeMenu();
+            }
+        });
+    } else { // Desktop: Hover behavior
+        menu_icon.addEventListener('mouseover', showHover);
+        hover_icon.addEventListener('mouseover', showHover);
+
+        menu_icon.addEventListener('mouseout', hideHoverDelayed);
+        hover_icon.addEventListener('mouseout', hideHover);
+    }
+}
+
+// Helper functions
+function toggleMenu(event) {
+    event.stopPropagation(); // Prevent closing immediately when clicking menu_icon
+
+    if (hover_icon.style.display === "block") {
+        closeMenu();
+    } else {
+        hover_icon.style.display = "block";
+        menu_icon.style.height = "0px";
+        menu_icon.style.width = "0px";
+    }
+}
+
+function closeMenu() {
+    hover_icon.style.display = "none";
+    menu_icon.style.height = "35px";
+    menu_icon.style.width = "35px";
+}
+
+function showHover() {
     hover_icon.style.display = "block";
     menu_icon.style.height = "0px";
     menu_icon.style.width = "0px";
-});
-hover_icon.addEventListener('mouseover', () => {
-    hover_icon.style.display = "block";
-    menu_icon.style.height = "0px";
-    menu_icon.style.width = "0px";
-});
-menu_icon.addEventListener('mouseout', () => {
+}
+
+function hideHoverDelayed() {
     setTimeout(() => {
         if (!hover_icon.matches(':hover')) {
-            hover_icon.style.display = "none";
-            menu_icon.style.display = "block";
-            menu_icon.style.height = "35px";
+            closeMenu();
+            menu_icon.style.display = "block"; // in case it was hidden
         }
     }, 1000);
-});
+}
 
-hover_icon.addEventListener('mouseout', () => {
-    hover_icon.style.display = "none"; 
+function hideHover() {
+    hover_icon.style.display = "none";
     menu_icon.style.height = "35px";
-});
+    menu_icon.style.width = "35px";
+}
+
+// Initialize
+setupListeners();
+
